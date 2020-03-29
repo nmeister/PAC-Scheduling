e3cab611107c5a8ebfac8d9a05244c5c5475e985
Helen Chen <hc20@princeton.edu>
2020-03-29 15:07:53 -0400

    basic stuff

---
+++ pacSite/pacApp/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse 
+from django.template.loader import render_to_string 
+# Create your views here.
+
+def index(request):
+	#return HttpResponse("Hello, World")
+	html = render_to_string('home.html')
+	return HttpResponse(html)
