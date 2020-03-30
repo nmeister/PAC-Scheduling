12d76d892fa60432aaa3fb7d5bf9bad1a82293b2
nmeister <nmeister@princeton.edu>
2020-03-29 20:27:27 -0400

    heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn pacSite.pacSite.wsgi --log-file - 
--- pacSite/pacSite/wsgi.py
+++ pacSite/pacSite/wsgi.py
@@ -16,3 +16,4 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pacSite.settings')
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
+# hello 
