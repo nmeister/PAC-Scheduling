d7e2620baa381afc683c37e9fc2c92ebcd7e9610
Helen Chen <hc20@princeton.edu>
2020-04-01 18:17:19 -0400

    model TimeSlot

--- pacApp/views.py
+++ pacApp/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 from django.template.loader import render_to_string 
 from django.conf.urls.static import static
+from . import models
+
 
 # Create your views here.
 
@@ -11,4 +13,7 @@ def index(request):
 	return HttpResponse(html)
 
 def schedule(request):
-	return render(request, "templates/pacApp/home2.html")
+	t1 = models.TimeSlot(company_name="Sympoh", time_start=5, time_end=7,
+		studio="Wilcox")
+	context = {t1.company_name:t1}
+	return render(request, "templates/pacApp/home2.html", context)
