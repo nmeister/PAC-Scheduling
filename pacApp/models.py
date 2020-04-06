0821a09436191466a192bcf75715bde1ccbe1925
Helen Chen <hc20@princeton.edu>
2020-04-06 09:32:06 -0400

    database

--- pacApp/models.py
+++ pacApp/models.py
@@ -8,3 +8,11 @@ class TimeSlot(models.Model):
 	time_start = models.IntegerField()
 	time_end = models.IntegerField()
 	studio = models.CharField(max_length=50)
+
+
+
+class Hours(models.Model):
+	start = models.IntegerField()
+	end = models.IntegerField()
+	duration = models.IntegerField()
+
